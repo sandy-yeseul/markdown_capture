189d667299578b339048d9e1fc63cad90482f407
sandy-yeseul <yeseulkang0328@gmail.com>
2021-04-07 02:02:56 +0900

    able to get book data and able to contain book data in one array(demo)

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 const puppeteer = require("puppeteer");
 
 async function getList(){
-    const url               = "https://ridibooks.com/keyword-finder/romance?set_id=1";
+    const url               = "https://ridibooks.com/keyword-finder/romance?order=recent&page=1&set_id=1";
     const sampleTagXpath    = '//*[@id="KeywordFinderRenewal"]/div[1]/fieldset[6]/div/div/div[1]/ul/li[3]/label/span'; //단행본 태그
     const markdownTagXpath  = '//*[@id="KeywordFinderRenewal"]/div[1]/fieldset[6]/div/div/div[1]/ul/li[2]/label/span';
     const resultDivXpath    = '//*[@id="KeywordFinderRenewal"]/div[2]/div';
     const resultHeaderXpath = '//*[@id="KeywordFinderRenewal"]/div[2]/div/header/div[1]';
     const emptyHeaderXpath  = '//*[@id="KeywordFinderRenewal"]/div[2]/div/p';
     const ResultCountXpath  = '//*[@id="KeywordFinderRenewal"]/div[2]/div/header/div[1]';
+    const bookClassSelector = '.RSGBookMetadata';
+    const titleClassSelector= '.RSGBookMetadata_Title';
+    const authorClassSelector= '.RSGBookMetadata_Authors';
+    const priceClassSelector= '.RSGBookMetadata_Price_CurrentPrice';
 
     try{
         const browser = await puppeteer.launch();
@@ -35,9 +39,40 @@ async function getList(){
         console.log(title);
         */
 
-        const titles = await page.$$eval('.RSGBookMetadata_Title', (els) => els.map(el => el.innerHTML));
-        console.log(typeof titles);
+        const titles = await page.$$eval(titleClassSelector, (titles) => titles.map(title => title.innerHTML)); // get title list only
         
+        /** // get book from metadata by $eval
+        const book = await page.$eval(bookClassSelector, data=> 
+                data.querySelector('.RSGBookMetadata_Title').textContent 
+        + ' ' + data.querySelector('.RSGBookMetadata_Authors').textContent
+        + ' ' + data.querySelector('.RSGBookMetadata_Price_CurrentPrice').textContent
+        ); 
+        console.log(book)
+        */
+         
+        // ANCHOR get book list -> book contain title, author, price
+        // STUB problems: 
+        // price has two option, one book price and set books price
+        // need books array to contain all books from different pages
+        // need to distinguish between title, author and price
+
+        const books = await page.$$eval(bookClassSelector, bookElems =>
+            bookElems.map(
+                bookElem => bookElem.querySelector('.RSGBookMetadata_Title').textContent 
+                + ' ' + bookElem.querySelector('.RSGBookMetadata_Authors').textContent
+                + ' ' + bookElem.querySelector('.RSGBookMetadata_Price_CurrentPrice').textContent
+            ));
+
+        console.log(books)
+        
+        // const books = 
+        //     await page.$$eval(bookClassSelector, booksElem => 
+        //     booksElem.map(bookElem => 
+        //     bookElem.querySelector(titleClassSelector).textContent
+        //     + bookElem.querySelector(authorClassSelector).textContent 
+        //     + bookElem.querySelector(priceClassSelector).textContent))
+        
+        // console.log
         
         // const prices = await page.$$eval('.RSGBookMetadata_Price_CurrentPrice', span => span.length)
         // console.log(prices);
