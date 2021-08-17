e264a4f82c82bb1ba7d051445f7ef3ce6cd6a42f
sandy-yeseul <yeseulkang0328@gmail.com>
2021-08-17 21:39:14 +0900

    able to post test tweet

--- twitter.js
+++ twitter.js
@@ -2,14 +2,16 @@ const Twitter = require('twitter');
 require('dotenv').config();
 
 const client = new Twitter({
-    consumer_key: '',
-    consumer_secret: '',
-    access_token_key: '',
-    access_token_secret: ''
+    consumer_key: process.env.TWITTER_CONSUMER_KEY,
+    consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+    access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
+    access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
 });
 
 let params = {screen_name: 'nodejs'};
-client.get('satuses/user_timeline', params, (err, tweets, response)=>{
-    if(err) return console.log(err);
-    console.log(tweets)
-})
+
+client.post('statuses/update', {status: 'I Love Twitter'},  function(error, tweet, response) {
+    if(error) throw error;
+    console.log(tweet);  // Tweet body.
+    console.log(response);  // Raw response object.
+  });
