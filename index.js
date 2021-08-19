f5b7729c5be4d8c94b809b009c651c9aca39fbe7
sandy-yeseul <yeseulkang0328@gmail.com>
2021-08-19 23:31:03 +0900

    cannot go to next page

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
 import {sendTweet} from './twitter.js';
-import { getBookList } from './scraper.js';``
+import { getBookList } from './scraper.js';
+
+async function runScript(){
+    // for (let i = 0; i <= 2; i++) {
+        
+    // }
+    const books = await getBookList(0);
+        console.log(books)
+}
+runScript();
--- scraper.js
+++ scraper.js
@@ -2,6 +2,7 @@ import puppeteer from 'puppeteer';
 
 const url               = `https://ridibooks.com/keyword-finder/romance?order=recent&page=1&set_id=1`;
 const sampleTagXpath    = '//*[@id="KeywordFinderRenewal"]/div[1]/fieldset[3]/div/div/div[1]/ul/li[20]/label/span'; //여공남수 태그
+const glTagXpath        = `//*[@id="KeywordFinderRenewal"]/div[1]/fieldset[1]/div/div/div[1]/ul/li[12]/label/span`;
 const markdownTagXpath  = '//*[@id="KeywordFinderRenewal"]/div[1]/fieldset[6]/div/div/div[1]/ul/li[2]/label/span';
 const resultDivXpath    = '//*[@id="KeywordFinderRenewal"]/div[2]/div';
 const resultHeaderXpath = '//*[@id="KeywordFinderRenewal"]/div[2]/div/header/div[1]';
@@ -17,19 +18,51 @@ export async function getBookList(pageCount){
       const browser = await puppeteer.launch();
       const page = await browser.newPage();
       await page.goto(url);
-      const [getXpath] = await page.$x(sampleTagXpath);
+      const [getXpath] = await page.$x(glTagXpath);
       await getXpath.click();
-
-      for (var i = 0; i++; i <= pageCount) {
-        const nextBtnIcon = await page.$(".RSGIcon-arrowRight");
-        if(nextBtnIcon === null) {return false;} // NOTE to stop the loop and return false to index to stop the loop
-        const nextBtn = await nextBtnIcon.getProperty("parentNode");
-        nextBtn.asElement().click();
-        await page.waitForTimeout(1000); // NOTE or else they repeat page
-      }
-
       await page.waitForXPath(ResultCountXpath);
-      const getUrl = page.url();
+
+      const nextBtn = await page.$('.Pagination:last-child');
+      await nextBtn.click();
+      const url = await page.url();
+      console.log(url)
+      return;
+
+
+    // console.log("why it's not working")
+    // const nextBtn = await page.$eval('.Pagination', (parentUlTag) =>{
+    //     let nextBtnElem = parentUlTag.lastElementChild;
+    //     if(nextBtnElem.tagName ==="DIV") {return false};
+    //     console.log(`inside ${nextBtnElem}`)
+    //     return nextBtnElem;
+    // })
+    // if(!nextBtn) return false;
+    // nextBtn.click();
+
+    //   for (var i = 0;  i <pageCount; i++) {
+    //     const nextBtnIcon = await page.$(".RSGIcon-arrowRight");
+    //     if(nextBtnIcon === null) {return false;} // NOTE to stop the loop and return false to index to stop the loop
+    //     const nextBtn = await nextBtnIcon.getProperty("parentNode");
+    //     nextBtn.asElement().click();
+    //     await page.waitForTimeout(1000); // NOTE or else they repeat page
+    //   }
+    
+    // const aTag = "A";
+    // const divTag = "DIV";
+    // for (let i = 0; i < pageCount; i++) {
+    //   let isContinued = await page.evaluate(async () => {
+    //     await page.waitForXPath(ResultCountXpath);
+    //     let nextBtn = document.querySelector(".Pagination").lastElementChild;
+    //     if (nextBtn.tagName === divTag) {
+    //       return false;
+    //     }
+    //     nextBtn.click();
+    //     return true;
+    //   });
+    //   if (!isContinued) return false;
+    // }
+
+
 
       const books = await page.$$eval(bookClassSelector, (bookElems) =>
         bookElems.map((bookElem) => {
