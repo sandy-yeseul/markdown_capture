4b28c0236f7cf8cca8f9d4521d6a21e6820027a7
sandy-yeseul <yeseulkang0328@gmail.com>
2021-08-21 01:39:59 +0900

    try to separate nextpage, and try to pass out scope value inside eval function

--- scraper.js
+++ scraper.js
@@ -20,7 +20,8 @@ const bookHelper = {
   author: authorClassSelector,
   priceParent: priceParentClassSelector,
   price1: priceClassSelector1,
-  price2: priceClassSelector2
+  price2: priceClassSelector2,
+  makeBook: makeBook
 }
 
 export async function getBookList(pageCount){
@@ -32,69 +33,24 @@ export async function getBookList(pageCount){
       await getXpath.click();
       await page.waitForXPath(ResultCountXpath);
 
-      // const nextBtn = await page.$('.Pagination:last-child');
-      // await nextBtn.click();
-      // return;
-
-
-    // console.log("why it's not working")
-    // const nextBtn = await page.$eval('.Pagination', (parentUlTag) =>{
-    //     let nextBtnElem = parentUlTag.lastElementChild;
-    //     if(nextBtnElem.tagName ==="DIV") {return false};
-    //     console.log(`inside ${nextBtnElem}`)
-    //     return nextBtnElem;
-    // })
-    // if(!nextBtn) return false;
-    // nextBtn.click();
-
-    //   for (var i = 0;  i <pageCount; i++) {
-    //     const nextBtnIcon = await page.$(".RSGIcon-arrowRight");
-    //     if(nextBtnIcon === null) {return false;} // NOTE to stop the loop and return false to index to stop the loop
-    //     const nextBtn = await nextBtnIcon.getProperty("parentNode");
-    //     nextBtn.asElement().click();
-    //     await page.waitForTimeout(1000); // NOTE or else they repeat page
-    //   }
-    
-    // const aTag = "A";
-    // const divTag = "DIV";
-    // for (let i = 0; i < pageCount; i++) {
-    //   let isContinued = await page.evaluate(async () => {
-    //     await page.waitForXPath(ResultCountXpath);
-    //     let nextBtn = document.querySelector(".Pagination").lastElementChild;
-    //     if (nextBtn.tagName === divTag) {
-    //       return false;
-    //     }
-    //     nextBtn.click();
-    //     return true;
-    //   });
-    //   if (!isContinued) return false;
-    // }
-      const ab = await page.$$eval(bookClassSelector, (bookElems)=>{
-        bookElems.map(bookElem => {
-          const title = bookElem.querySelector(bookHelper.title).textContent;
-          return title;
-        }, bookHelper)
-      }, bookHelper)
-      console.log("working maybe")
-      console.log(ab)
-      // const books = await page.$$eval(bookClassSelector, (bookElems) =>
-      //   bookElems.map((bookElem) => {
-      //     const title = bookElem.querySelector(bookHelper.title).textContent;
-      //     const author = bookElem.querySelector(".RSGBookMetadata_Authors").textContent;
-      //     const salePrice = bookElem.querySelector("ul.RSGBookMetadata_Price_Row")
-      //                   .childElementCount > 1 
-      //                   ? bookElem.querySelector("li:nth-child(2) > span.RSGBookMetadata_Price_CurrentPrice").textContent
-      //                   : bookElem.querySelector(".RSGBookMetadata_Price_CurrentPrice").textContent;
-      //     const book = makeBook({
-      //       title: title,
-      //       author: author,
-      //       salePrice: salePrice,
-      //     });
-      //     return book;
-      //   })
-      // );
+      const books = await page.$$eval(bookClassSelector,
+        (bookElems, bookHelper) => {
+          bookElems.map((bookElem) => {
+            const title = bookElem.querySelector(bookHelper.title).textContent;
+            const author = bookElem.querySelector(bookHelper.author).textContent;
+            const salePrice = bookElem.querySelector(bookHelper.priceParent).childElementCount > 1
+                            ? bookElem.querySelector(bookHelper.price1).textContent
+                            : bookElem.querySelector(bookHelper.price2).textContent;
+            const book = {
+              title: title,
+              author: author,
+              salePrice: salePrice,
+            };
+            return book;
+          });
+        },bookHelper);
       await browser.close();
-      // return books;
+      console.log(books)
     } catch (err) {
       
     }
