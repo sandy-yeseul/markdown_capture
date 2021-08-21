2859951463f1ecaea417fa2c5b58ec11d88ef1da
sandy-yeseul <yeseulkang0328@gmail.com>
2021-08-21 19:46:01 +0900

    able to send initial tweet

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 import { getBookList } from './scraper.js';
+import {sendIntialTweet} from './tweet.js'
 
 async function runScript(){
-    const page = 0
-    const books = await getBookList(page);
-    console.log(books)
+    // const page = 0
+    // const books = await getBookList(page);
+    // console.log(books)
+
+    const period = `2021년 8월 23일 수요일 ~ 31일 금요일`
+    try {
+        const replyId = await sendIntialTweet(period)
+        console.log(replyId)
+        console.log(replyId.id)
+    } catch (error) {
+        console.log(error)
+    }
 }
 runScript();
--- tweet.js
+++ tweet.js
@@ -1,24 +1,44 @@
 
 import { getBookList } from './scraper.js';
-import { tweet } from './twitter.js';
+import { tweet as twitter } from './twitter.js';
 
 
-let params ={ screen_name: 'ridi_MARKDOWN', }
+let twitter_params ={ screen_name: 'ridi_MARKDOWN', }
 
 
-export async function sendTweet(data){
+export async function sendIntialTweet(period){
     const dateObj = new Date();
     const year = dateObj.getUTCFullYear();
     const month = dateObj.getUTCMonth() +1;
-    const page = 0
-    const books = await getBookList(page);
-    const bookStr = `${year}년 ${month}월 마크다운\n《${books[3].title}》\n${books[3].author}\n${books[3].salePrice}\n${books[3].link}`
-     
-    const tweetParams = {trim_user: true}
-    tweet.post('statuses/update', {status: bookStr, in_reply_to_status_id: '1429008326432477186', tweetParams}, (err, tweet, result)=>{
-        if(err) console.log(err)
-        console.log(tweet)
-    });
-    
+    const initialTweet = `${year}년 ${month}월 마크다운 리스트\n기간: ${period}\n@${twitter_params.screen_name}`
+    try {
+        const tweet = await twitter.post('statuses/update', {status: initialTweet});
+        return tweet;
+    } catch (error) {
+        console.log(error)
+    }
 }
-sendTweet(params)
+
+// export async function replyTweet(replyId, tweetData){
+//     try {
+//         const tweetResult = await twitter.post('statuses/update', {status: initialTweet, trim_user: true});
+//     } catch (error) {
+//         console.log(error)
+//     }
+// }
+
+// export async function sendTweet(data){
+//     let replyId = ''
+//     try {
+//         const tweetResult = await twitter.post('statuses/update', {status: initialTweet, trim_user: true});
+//         console.log(tweetResult)
+//     } catch (error) {
+//         console.log(error)
+//     }
+    
+// }
+// function sendTweetThread(twitter, replyId){
+//     const page = 0
+//     const books = await getBookList(page);
+//     const bookStr = `${year}년 ${month}월 마크다운\n《${books[3].title}》\n${books[3].author}\n${books[3].salePrice}\n${books[3].link}`
+// }
