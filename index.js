e2da41c57a5dac618ce481295ceeb9861cac38ae
sandy-yeseul <yeseulkang0328@gmail.com>
2021-08-21 16:34:12 +0900

    able to send tweet !

--- index.js
+++ index.js
@@ -2,10 +2,8 @@ import {sendTweet} from './twitter.js';
 import { getBookList } from './scraper.js';
 
 async function runScript(){
-    // for (let i = 0; i <= 2; i++) {
-        
-    // }
-    const books = await getBookList(0);
-        console.log(books)
+    const page = 0
+    const books = await getBookList(page);
+    console.log(books)
 }
 runScript();
--- twitter.js
+++ twitter.js
@@ -1,6 +1,7 @@
 import Twitter from 'twitter';
 import dotenv from 'dotenv';
 dotenv.config();
+import { getBookList } from './scraper.js';
 
 const client = new Twitter({
     consumer_key: process.env.TWITTER_CONSUMER_KEY,
@@ -13,9 +14,14 @@ let params = {screen_name: 'nodejs'};
 
 
 
+
 export async function sendTweet(data){
-    client.post('statuses/update', {status: data},  function(error, tweet, response) {
-        if(error) throw error;
-        return tweet;
-      });
+
+    const page = 0
+    const books = await getBookList(page);
+    const book = `《${books[0].title}》 \n${books[0].author} \n${books[0].salePrice} \n${books[0].link}`
+    client.post('statuses/update', {status: book}, (err, tweet, result)=>{
+        
+    });
 }
+sendTweet(params)
