18366ae6bd9c80de79ef59a2a456c63201987f8d
sandy-yeseul <yeseulkang0328@gmail.com>
2021-10-21 21:37:54 +0900

    book model refactoring + test file added

--- __test/book.spec.js
+++ __test/book.spec.js
@@ -1,20 +1,76 @@
-const { expect } = require("chai");
+import {expect} from 'chai'
+import buildBook from '../model/book.js'
 
 describe("testing book model", ()=>{
     describe("null checking", ()=>{
-        it("must throw error: no title", ()=>{})
-        it("must throw error: no author", ()=>{})
+        it("must throw error: no title", ()=>{
+            expect(()=>buildBook())
+            .to.throw(Error);
+        })
+        it("must throw error: no author", ()=>{
+            expect(()=> buildBook({title: "title"}))
+            .to.throw(Error);
+        })
         // it("must throw error: no publisher", ()=>{})
-        it("must throw error: no sale price", ()=>{})
+        it("must throw error: no sale price", ()=>{
+            expect(()=>buildBook({title: "title", author: "author"}))
+            .to.throw(Error)
+        })
         // it("must throw error: no price", ()=>{})
     })
     describe("format checking", ()=>{
-        it("must throw error: title not string", ()=>{})
-        it("must throw error: author not string", ()=>{})
+        it("must throw syntax error: title not string", ()=>{
+            expect(()=>buildBook({title: {}, author: "author", salePrice: 10}))
+            .to.throw(SyntaxError)
+        })
+        it("must throw syntax error: author not string", ()=>{
+            expect(()=>buildBook({title: "title", author: 10, salePrice: 10}))
+            .to.throw(SyntaxError)
+        })
         // it("must throw error: publisher not string", ()=>{})
-        it("must throw error: sale price not number", ()=>{})
-        it("must throw error: sale price not whoel number", ()=>{})
+        it("must throw syntax error: sale price not string", ()=>{
+            expect(()=>buildBook({title: "title", author: "author", salePrice: []}))
+            .to.throw(SyntaxError);
+        })
+        it("must throw syntax error: sale price not whoel number", ()=>{
+            expect(()=>buildBook({title: "title", author: "author", salePrice: -10}))
+            .to.throw(SyntaxError) &&
+            expect(()=>buildBook({title: "title", author: "author", salePrice: 10.5}))
+            .to.throw(SyntaxError)
+        })
         // it("must throw error: price not number", ()=>{})
         // it("must throw error: price not whoel number", ()=>{})
+    })
+    describe("normal book model", ()=>{
+        const book = buildBook({title: "title", author: "author", salePrice: "12,500"})
+        it("must be frozen object", ()=>{
+            expect(book).to.be.an("object").and.to.be.frozen
+        })
+        it("should have _id", ()=>{
+            expect(book).to.have.property("_id")
+            .and.to.be.a("string")
+            .and.to.be.lengthOf.at.least(2)
+        })
+        it("should have title", ()=>{
+            expect(book).to.have.property("title")
+            .and.to.be.a("string")
+            .and.to.be.lengthOf.at.least(2)
+        })
+        it("should have author", ()=>{
+            expect(book).to.have.property("author")
+            .and.to.be.a("string")
+            .and.to.be.lengthOf.at.least(2)
+        })
+        it("should have publisher", ()=>{
+            expect(book).to.have.property("publisher")
+        })
+        it("should have salePrice", ()=>{
+            expect(book).to.have.property("salePrice")
+            .and.to.be.a("string")
+            .and.to.be.lengthOf.at.least(2)
+        })
+        it("should have price", ()=>{
+            expect(book).to.have.property("price")
+        })
     })
 })
--- model/book.js
+++ model/book.js
@@ -1,21 +1,21 @@
-export default ({ title, author, salePrice, price = "" } = {}) => {
-  const book = Object.freeze({
-    title: title,
-    author: author,
-    salePrice: salePrice,
-    price: price,
-  });
-  return book;
-};
-export default function buildBook({generateId}){
-  return ({
+import { v4 as uuidv4 } from "uuid"
+// export default ({ title, author, salePrice, price = "" } = {}) => {
+//   const book = Object.freeze({
+//     title: title,
+//     author: author,
+//     salePrice: salePrice,
+//     price: price,
+//   });
+//   return book;
+// };
+export default function buildBook({
     _id,
     title,
     author,
     publisher,
     salePrice,
     price
-  })=>{
+  }){
     //ANCHOR null checking
     if(!title) throw new Error("제목이 없습니다.")
     if(!author) throw new Error("작가가 없습니다.")
@@ -23,10 +23,27 @@ export default function buildBook({generateId}){
     if(!salePrice) throw new Error("할인 가격이 없습니다.")
 
     // ANCHOR format checking
+    if(typeof title !== "string") throw new SyntaxError("제목이 스트링이 아닙니다.")
+    if(typeof author !== "string") throw new SyntaxError("작가가 스트링이 아닙니다.")
+    // if(typeof publisher !== "string") throw new SyntaxError("출판사가 스트링이 아닙니다.")
+    if(typeof salePrice !== "string") throw new SyntaxError("할인 가격이 스트링이 아닙니다.")
+    if(Number(salePrice) < 1 || Number(salePrice) % 1 < 1) throw new SyntaxError("할인 가격이 자연수가 아닙니다.")
+    
+    //ANCHOR format data
+    if(!_id) _id = generateId();
 
-    if(typeof title !== "string") throw new Error("제목이 스트링이 아닙니다.")
-    if(typeof author !== "string") throw new Error("작가가 스트링이 아닙니다.")
-    // if(typeof publisher !== "string") throw new Error("출판사가 스트링이 아닙니다.")
-    if(typeof salePrice !== "number") throw new Error("할인 가격이 숫자가 아닙니다.")
+    const book = Object.freeze({
+      _id,
+      title,
+      author,
+      publisher,
+      salePrice,
+      price
+    })
+    return book;
   }
-}
+  function generateId(){
+    const uuid = uuidv4();
+    const id = uuid.replace(/-/g, "");
+    return id;
+  }
