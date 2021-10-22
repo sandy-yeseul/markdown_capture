b046cb68cc17154fde3fde2e70dc035bdfc1f3bd
sandy-yeseul <yeseulkang0328@gmail.com>
2021-10-23 00:57:20 +0900

    link and volume added

--- model/book.js
+++ model/book.js
@@ -1,33 +1,34 @@
 import { v4 as uuidv4 } from "uuid"
-// export default ({ title, author, salePrice, price = "" } = {}) => {
-//   const book = Object.freeze({
-//     title: title,
-//     author: author,
-//     salePrice: salePrice,
-//     price: price,
-//   });
-//   return book;
-// };
-export default function buildBook({
-    _id,
-    title,
-    author,
-    publisher,
-    salePrice,
-    price
-  }){
+const makeBook = buildBook(generateId);
+export {makeBook}
+function buildBook(generateId){ 
+  return({
+  _id,
+  title,
+  author,
+  salePrice,
+  link,
+  volume
+})=>{
     //ANCHOR null checking
     if(!title) throw new Error("제목이 없습니다.")
     if(!author) throw new Error("작가가 없습니다.")
-    // if(!publisher) throw new Error("출판사가 없습니다.")
     if(!salePrice) throw new Error("할인 가격이 없습니다.")
+    if(!link) throw new Error("링크가 없습니다.")
+    if(!volume) throw new Error("몇 권인지 표기해 주세요.")
 
     // ANCHOR format checking
     if(typeof title !== "string") throw new SyntaxError("제목이 스트링이 아닙니다.")
     if(typeof author !== "string") throw new SyntaxError("작가가 스트링이 아닙니다.")
-    // if(typeof publisher !== "string") throw new SyntaxError("출판사가 스트링이 아닙니다.")
     if(typeof salePrice !== "string") throw new SyntaxError("할인 가격이 스트링이 아닙니다.")
-    if(Number(salePrice) < 1 || Number(salePrice) % 1 < 1) throw new SyntaxError("할인 가격이 자연수가 아닙니다.")
+    if(!Number(salePrice.replace(/,/g, ''))) throw new SyntaxError("할인 가격이 올바른 숫자가 아닙니다.")
+    if(Number(salePrice.replace(/,/g, '')) % 1 !== 0) throw new SyntaxError("할인 가격이 정수가 아닙니다.")
+    if(Number(salePrice.replace(/,/g, '')) < 0) throw new SyntaxError("할인 가격이 음수입니다.")
+    if(typeof link !== "string") throw new SyntaxError("링크가 스트링이 아닙니다.")
+    if(typeof volume !=="string") throw new SyntaxError("책 권수가 스트링이 아닙니다.")
+    if(!Number(volume.replace(/,/g, ''))) throw new SyntaxError("책 권수가 올바른 숫자가 아닙니다.")
+    if(Number(volume.replace(/,/g, '')) % 1 !== 0) throw new SyntaxError("책 권수가 정수가 아닙니다.")
+    if(Number(volume.replace(/,/g, '')) < 1) throw new SyntaxError("책 권수가 음수입니다.") 
     
     //ANCHOR format data
     if(!_id) _id = generateId();
@@ -36,12 +37,13 @@ export default function buildBook({
       _id,
       title,
       author,
-      publisher,
       salePrice,
-      price
+      link,
+      volume
     })
     return book;
   }
+}
   function generateId(){
     const uuid = uuidv4();
     const id = uuid.replace(/-/g, "");
--- model/book.spec.js
+++ model/book.spec.js
@@ -1,48 +1,77 @@
 import {expect} from 'chai'
-import buildBook from './book.js'
+import {makeBook} from './book.js'
 
 describe("testing book model", ()=>{
     describe("null checking", ()=>{
         it("must throw error: no title", ()=>{
-            expect(()=>buildBook())
-            .to.throw(Error);
+            expect(()=>makeBook({}))
+            .to.throw(Error, "제목이 없습니다.");
         })
         it("must throw error: no author", ()=>{
-            expect(()=> buildBook({title: "title"}))
-            .to.throw(Error);
+            expect(()=> makeBook({title: "title"}))
+            .to.throw(Error, "작가가 없습니다.");
         })
-        // it("must throw error: no publisher", ()=>{})
         it("must throw error: no sale price", ()=>{
-            expect(()=>buildBook({title: "title", author: "author"}))
-            .to.throw(Error)
+            expect(()=>makeBook({title: "title", author: "author"}))
+            .to.throw(Error, "할인 가격이 없습니다.")
         })
-        // it("must throw error: no price", ()=>{})
+        it("must throw error: no link", ()=>{
+            expect(()=>makeBook({title: "title", author: "author", salePrice: "10"}))
+            .to.throw(Error, "링크가 없습니다.")
+        })
+        it("must throw error: no volume", ()=>{
+            expect(()=>makeBook({title: "title", author:"author", salePrice: "10", link: "link"}))
+            .to.throw(Error, "몇 권인지 표기해 주세요.")
+        })
     })
     describe("format checking", ()=>{
         it("must throw syntax error: title not string", ()=>{
-            expect(()=>buildBook({title: {}, author: "author", salePrice: 10}))
-            .to.throw(SyntaxError)
+            expect(()=>makeBook({title: {}, author: "author", salePrice: "10", link: "link", volume: "3"}))
+            .to.throw(SyntaxError, "제목이 스트링이 아닙니다.")
         })
         it("must throw syntax error: author not string", ()=>{
-            expect(()=>buildBook({title: "title", author: 10, salePrice: 10}))
-            .to.throw(SyntaxError)
+            expect(()=>makeBook({title: "title", author: 20, salePrice: "10", link: "link", volume: "3"}))
+            .to.throw(SyntaxError, "작가가 스트링이 아닙니다.")
         })
-        // it("must throw error: publisher not string", ()=>{})
         it("must throw syntax error: sale price not string", ()=>{
-            expect(()=>buildBook({title: "title", author: "author", salePrice: []}))
-            .to.throw(SyntaxError);
+            expect(()=>makeBook({title: "title", author: "author", salePrice: [], link: "link", volume: "3"}))
+            .to.throw(SyntaxError, "할인 가격이 스트링이 아닙니다.");
         })
-        it("must throw syntax error: sale price not whoel number", ()=>{
-            expect(()=>buildBook({title: "title", author: "author", salePrice: -10}))
-            .to.throw(SyntaxError) &&
-            expect(()=>buildBook({title: "title", author: "author", salePrice: 10.5}))
-            .to.throw(SyntaxError)
+        it("must throw syntax error: sale price not correct number", ()=>{
+            expect(()=>makeBook({title: "title", author: "author", salePrice: "10by", link: "link", volume: "3"}))
+            .to.throw(SyntaxError, "할인 가격이 올바른 숫자가 아닙니다.")
         })
-        // it("must throw error: price not number", ()=>{})
-        // it("must throw error: price not whoel number", ()=>{})
+        it("must throw syntax error: sale price not integer", ()=>{
+            expect(()=> makeBook({title: "title", author: "author", salePrice: "10.5", link: "link", volume: "3"}))
+            .to.throw(SyntaxError, "할인 가격이 정수가 아닙니다.")
+        })
+        it("must throw sytnax error: sale price below 0", ()=>{
+            expect(()=> makeBook({title: "title", author: "author", salePrice: "-10", link: "link", volume: "3"}))
+            .to.throw(SyntaxError, "할인 가격이 음수입니다.")
+        })
+        it("must throw syntax error: link not string", ()=>{
+            expect(()=>makeBook({title: "title", author: "author", salePrice: "10", link: {}, volume: "3"}))
+            .to.throw(SyntaxError, "링크가 스트링이 아닙니다.")
+        })
+        it("must throw syntax error: volume not string", ()=>{
+            expect(()=>makeBook({title: "title", author: "author", salePrice: "10", link: "link", volume: ["3"]}))
+            .to.throw(SyntaxError, "책 권수가 스트링이 아닙니다.")
+        })
+        it("must throw syntax error: volume not correct number", ()=>{
+            expect(()=>makeBook({title: "title", author: "author", salePrice: "10", link: "link", volume: "ah1.1"}))
+            .to.throw(SyntaxError, "책 권수가 올바른 숫자가 아닙니다.")
+        })
+        it("must throw syntax error: volume not integer", ()=>{
+            expect(()=>makeBook({title: "title", author: "author", salePrice: "10", link: "link", volume: "3.5"}))
+            .to.throw(SyntaxError, "책 권수가 정수가 아닙니다.")
+        })
+        it("must throw syntax error: volume below 0", ()=>{
+            expect(()=>makeBook({title: "title", author: "author", salePrice: "10", link: "link", volume: "-10"}))
+            .to.throw(SyntaxError, "책 권수가 음수입니다.")
+        })
     })
     describe("normal book model", ()=>{
-        const book = buildBook({title: "title", author: "author", salePrice: "12,500"})
+        const book = makeBook({title: "title", author: "author", salePrice: "10,700", link: "link", volume: "3"})
         it("must be frozen object", ()=>{
             expect(book).to.be.an("object").and.to.be.frozen
         })
@@ -61,16 +90,19 @@ describe("testing book model", ()=>{
             .and.to.be.a("string")
             .and.to.be.lengthOf.at.least(2)
         })
-        it("should have publisher", ()=>{
-            expect(book).to.have.property("publisher")
-        })
         it("should have salePrice", ()=>{
             expect(book).to.have.property("salePrice")
             .and.to.be.a("string")
             .and.to.be.lengthOf.at.least(2)
         })
-        it("should have price", ()=>{
-            expect(book).to.have.property("price")
+        it("should have link", ()=>{
+            expect(book).to.have.property("link")
+            .and.to.be.a("string")
+            .and.to.be.lengthOf.at.least(2)
+        })
+        it("should have volume", ()=>{
+            expect(book).to.have.property("volume")
+            .and.to.be.a("string")
         })
     })
 })
