799ff146143b5ccbec3a1d06d2d099248e2ba571
sandy-yeseul <yeseulkang0328@gmail.com>
2021-10-23 09:57:55 +0900

    able to open page

--- use-case/test.spec.js
+++ use-case/test.spec.js
@@ -1,7 +1,18 @@
+import { openPage } from "./scrape.js"
+
 describe("get data", ()=>{
     describe("scrape from web page", ()=>{
-        it("must open page", async()=>{})
-        it("must set xPath for require ")
+        it("must open page", async()=>{
+            const res = await openPage();
+            console.log(res)
+        })
+        it("must set xPath for require ", async()=>{})
+        it("must wait for page", async()=>{})
+        it("must get event period", async()=>{})
+        it("must get all book contents and make array", async()=>{})
+        it("must get 100% point back book information and filter out from array", async()=>{})
+        it("must get new book 6 and filter out from array", async()=>{})
+        it("must make book model for each book in array", async()=>{})
     })
 })
 describe("save data", ()=>{})
