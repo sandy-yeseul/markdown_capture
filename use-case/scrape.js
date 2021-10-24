df809ba5ffd0a14ca65ede2c6927e05689d04480
sandy-yeseul <yeseulkang0328@gmail.com>
2021-10-24 12:34:48 +0900

    able to get all authors

--- use-case/scrape.js
+++ use-case/scrape.js
@@ -21,8 +21,9 @@ async function test(){
             els.forEach(item => {
                 const volume = item.querySelector(".set_text").textContent;
                 const link = item.querySelector(".title_link").href;
-                const title = item.querySelector(".title_link > span").innerText
-                res.push(title)
+                const title = item.querySelector(".title_link").innerText;
+                const author = item.querySelector(".author").innerText;
+                res.push(author)
             })
             // const el = els.querySelectorAll(".set_text")
             // el.forEach(item => {res.push(item.textContent)})
