eb1b7a2bd2a2efa792e8f4a7fb226230a57b3d7c
sandy-yeseul <49422051+sandy-yeseul@users.noreply.github.com>
2022-02-11 20:09:08 +0900

    npm start changed

--- index.js
+++ index.js
@@ -1,13 +1,28 @@
-import { getBooks } from "./use-case/get-data.js";
-import { getTwitter, tweetInitialTweet } from "./use-case/publish-data.js";
+// import { getBookList } from "./scraper/scraper.js";
+// import { sendIntialTweet, replyTweet } from "./twitter/tweet.js";
 
-async function test(){
-  try {
-    // const result = await getBooks();
-    // console.log(result)
-   console.log(await tweetInitialTweet())
-  } catch (err) {
-    console.log(err)
-  }
-}
-test()
+// async function runScript() {
+//   let page = 0;
+//   let isContinued = true;
+//   const period = `2021.08.23.(월) 07:00 ~ 2021.08.31.(화) 23:59`;
+//   try {
+//     var replyId = await sendIntialTweet(period);
+//     while (isContinued) {
+//       const books = await getBookList(page);
+//       if(!books) {
+//           isContinued = false;
+//           console.log("DONE");
+//           break;
+//         };
+//       console.log(books);
+//       for (var i = 0; i < books.length; i++) {
+//         var replyId = await replyTweet(replyId, books[i]);
+//       }
+//       page++;
+//     }
+//     console.log("Loop is done")
+//   } catch (error) {
+//     console.log(error);
+//   }
+// }
+// runScript();
