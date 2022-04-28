504bf9cdf0220444a899fe00c9579742bc76a010
sandy-yeseul <49422051+sandy-yeseul@users.noreply.github.com>
2022-04-27 23:58:08 -0400

    testing saving data - worked

--- use-case/save-data.spec.js
+++ use-case/save-data.spec.js
@@ -3,9 +3,14 @@ import { markdownDb } from "../data-handler/db-handler.js";
 import { getBooks } from "./get-data.js"
 import { saveData } from "./save-data.js";
 
+let eventP
 describe("save data", ()=>{
+    after(async()=>{
+        await markdownDb.deleteBooksByEventPeriod(eventP)
+    })
     it("must save data and get inserted count", async()=>{
         const {markdownBookList, eventPeriod} = await getBooks();
+        eventP = eventPeriod.split(": ")[1]
         const insertedCount = await saveData(markdownBookList, eventPeriod);
         expect(insertedCount).to.be.greaterThan(100);
     })
