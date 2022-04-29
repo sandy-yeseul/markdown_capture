20c3aca12caa22088642d159de9142e90764a974
sandy-yeseul <49422051+sandy-yeseul@users.noreply.github.com>
2022-04-29 16:02:34 -0400

    make it to print error message

--- test.js
+++ test.js
@@ -1,11 +1,15 @@
 import { getTwitter } from "./use-case/publish-data.js";
 
 (async()=>{
-    const twitter = getTwitter();
+    try {
+        const twitter = getTwitter();
 
-    const content = new Date().toLocaleString();
+        const content = new Date().toLocaleString();
 
-    const {id_str} = await twitter.post("statuses/update", {
-        status: content
-    })
+        const {id_str} = await twitter.post("statuses/update", {
+            status: content
+        })
+    } catch (err) {
+        console.log(err)
+    }
 })();
