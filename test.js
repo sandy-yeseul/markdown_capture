25a4834d311b25fab798dbf58dfe040b9e0f04b5
sandy-yeseul <49422051+sandy-yeseul@users.noreply.github.com>
2022-04-30 10:50:17 -0400

    testing environment values

--- test.js
+++ test.js
@@ -4,6 +4,11 @@ import { getTwitter } from "./use-case/publish-data.js";
     try {
         const twitter = getTwitter();
 
+        console.log(process.env.TWITTER_CONSUMER_KEY)
+        console.log(process.env.TWITTER_CONSUMER_SECRET)
+        console.log(process.env.TWITTER_ACCESS_TOKEN_KEY)
+        console.log(process.env.TWITTER_ACCESS_TOKEN_SECRET)
+
         const content = new Date().toLocaleString();
 
         const {id_str} = await twitter.post("statuses/update", {
