00346daf5d1965b4679051668c7496e66c0f036d
sandy-yeseul <49422051+sandy-yeseul@users.noreply.github.com>
2022-05-02 21:27:04 -0400

    setting

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ import { saveData } from './use-case/save-data.js'
 
 async function runScript() {
   try {
-    console.log(new Date().toLocaleString())
     const scrapedData = await getBooks();
 
     if(scrapedData === false) return;
@@ -13,20 +12,15 @@ async function runScript() {
     const {markdownBookList, eventPeriod: eventP} = scrapedData;
 
     const eventPeriod = eventP.split(': ')[1]
-    console.log(eventPeriod)
-    console.log(new Date().toLocaleString())
 
     const hasBook = await markdownDb.findOneBookWithEventPeriod(eventPeriod)
     if(hasBook !== null && hasBook !== undefined) return;
-    console.log(new Date().toLocaleString() + 'done finding any book(아 혹시 return되나?')
 
     await saveData(markdownBookList, eventP);
-    console.log(new Date().toLocaleString() +'done saving')
 
     let replyId = await tweetInitialTweet(eventPeriod);
 
     const books = await markdownDb.findBooks(eventPeriod);
-    console.log(new Date().toLocaleString() +'done initial tweet')
 
     for(let i = 0; i<3; i++){
         const book = books[i]
@@ -34,7 +28,6 @@ async function runScript() {
         replyId = await replyTweet(replyId, book);
         await markdownDb.updateTweetId(book._id, replyId)
     }
-    console.log(new Date().toLocaleString())
   } catch (err) {
     console.log(err);
   }
