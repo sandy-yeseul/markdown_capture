4e9a9eab5120aa02cfe731d2fe33ac6a9c212613
sandy-yeseul <49422051+sandy-yeseul@users.noreply.github.com>
2022-05-02 21:50:13 -0400

    add try-catch for catching errors

--- use-case/get-data.js
+++ use-case/get-data.js
@@ -35,62 +35,80 @@ async function getBooks(){
     }
 }
 async function openPage(){
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage();
-    return page;
+    try {
+        const browser = await puppeteer.launch();
+        const page = await browser.newPage();
+        return page;
+    } catch (err) {
+        console.log(err)
+    }
 }
 
 async function gotToEventPage(page){
-    const url = 'https://ridibooks.com/event/romance'
-    await page.goto(url);
-    return;
+    try {
+        const url = 'https://ridibooks.com/event/romance'
+        await page.goto(url)
+    } catch (err) {
+        console.log(err)        
+    }
 }
 async function openMarkdownEventPage(page){
-    const eventDescriptionClassSelector = ".descript_body";
-    const eventName = "마크다운"
-    const hrefArr = await page.$$eval(eventDescriptionClassSelector, (descElems, eventName) => 
-        descElems.map(descElem =>{
-            if(descElem.textContent.includes(eventName)) {
-                
-                // link가 /event/35425 이렇게 나와서 나중에 숫자만 extract 함
-                return descElem.closest('.event_description_wrapper').querySelector('a').href
-            }
-        }), eventName
-    )
-    let markdownEventUrlNumber = hrefArr.filter(item => item)[0].match(/\d+/)[0] // 숫자 extract
-    const markDownPageUrl = `https://ridibooks.com/event/${markdownEventUrlNumber}`
-
-    await page.goto(markDownPageUrl);
-    return;
+    try {
+        const eventDescriptionClassSelector = ".descript_body";
+        const eventName = "마크다운"
+        const hrefArr = await page.$$eval(eventDescriptionClassSelector, (descElems, eventName) => 
+            descElems.map(descElem =>{
+                if(descElem.textContent.includes(eventName)) {
+                    
+                    // link가 /event/35425 이렇게 나와서 나중에 숫자만 extract 함
+                    return descElem.closest('.event_description_wrapper').querySelector('a').href
+                }
+            }), eventName
+        )
+        let markdownEventUrlNumber = hrefArr.filter(item => item)[0].match(/\d+/)[0] // 숫자 extract
+        const markDownPageUrl = `https://ridibooks.com/event/${markdownEventUrlNumber}`
+    
+        await page.goto(markDownPageUrl);
+    } catch (err) {
+        console.log(err)
+    }
 }
 async function getEventPeriod(page){
-    const noticeListSelector = ".notice_list";
-    const eventPeriod = await page.$$eval(noticeListSelector, (noticeListEls)=> {
-        return noticeListEls[1].firstElementChild.textContent
-    })
-    return eventPeriod;
+    try {
+        const noticeListSelector = ".notice_list";
+        const eventPeriod = await page.$$eval(noticeListSelector, (noticeListEls)=> {
+            return noticeListEls[1].firstElementChild.textContent
+        })
+        return eventPeriod;
+    } catch (err) {
+        console.log(err)
+    }
 }
 async function getMarkdownBooks(page){
-    const markdownListEl = await page.$$eval(".event_detail_book_list_wrapper", (listEls) => {
-        const books = [];
-        listEls.forEach(listEl => {
-           const makrdownEls = listEl.querySelectorAll(".book_macro_110")
-           makrdownEls.forEach(markdownEl => {
-            const volumeEl = markdownEl.querySelector(".set_text");
-            // 신작 6종 거르기 위해
-            if(volumeEl === null) return;
-            const volume = volumeEl.textContent;
-            const link = markdownEl.querySelector(".title_link").href;
-            const title = markdownEl.querySelector(".title_link").innerText;
-            const author = markdownEl.querySelector(".author").innerText;
-            const salePrice = markdownEl.querySelector(".price").innerText;
-            const book = {title, author, salePrice, link, volume};
-            books.push(book);
-            })
-        })        
-        return books;
-    })
-    return markdownListEl;
+    try {
+        const markdownListEl = await page.$$eval(".event_detail_book_list_wrapper", (listEls) => {
+            const books = [];
+            listEls.forEach(listEl => {
+               const makrdownEls = listEl.querySelectorAll(".book_macro_110")
+               makrdownEls.forEach(markdownEl => {
+                const volumeEl = markdownEl.querySelector(".set_text");
+                // 신작 6종 거르기 위해
+                if(volumeEl === null) return;
+                const volume = volumeEl.textContent;
+                const link = markdownEl.querySelector(".title_link").href;
+                const title = markdownEl.querySelector(".title_link").innerText;
+                const author = markdownEl.querySelector(".author").innerText;
+                const salePrice = markdownEl.querySelector(".price").innerText;
+                const book = {title, author, salePrice, link, volume};
+                books.push(book);
+                })
+            })        
+            return books;
+        })
+        return markdownListEl;
+    } catch (err) {
+        console.log(err)
+    }
 }
 async function checkMarkdownEvent(page){
     try {
--- use-case/save-data.js
+++ use-case/save-data.js
@@ -2,11 +2,15 @@ import { markdownDb } from '../data-handler/db-handler.js';
 import {makeBook} from '../model/book.js'
 export{saveData}
 async function saveData(markdownBookList, eventPeriod){
-    eventPeriod = eventPeriod.split(": ")[1]
-    const books = markdownBookList.map(book => {
-        book.eventPeriod = eventPeriod;
-        return makeBook(book);
-    });
-    const insertedCount = await markdownDb.insertManyBooks(books)
-    return insertedCount;
+    try {
+        eventPeriod = eventPeriod.split(": ")[1]
+        const books = markdownBookList.map(book => {
+            book.eventPeriod = eventPeriod;
+            return makeBook(book);
+        });
+        const insertedCount = await markdownDb.insertManyBooks(books)
+        return insertedCount;
+    } catch (err) {
+        console.log(err)
+    }
 }
